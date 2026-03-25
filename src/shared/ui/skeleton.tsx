618057871c3907278160a2ac7c257fade3aa2582
tukembaev <tukembaev.arif@gmail.com>
2026-03-25 12:03:24 +0600

    fix: skeleton blindness , taskdoctab deleted

--- src/shared/ui/skeleton.tsx
+++ src/shared/ui/skeleton.tsx
@@ -6,7 +6,7 @@ function Skeleton({
 }: React.HTMLAttributes<HTMLDivElement>) {
   return (
     <div
-      className={cn("animate-pulse rounded-md bg-muted", className)}
+      className={cn("rounded-md bg-slate-300 dark:bg-slate-700", className)}
       {...props}
     />
   )
