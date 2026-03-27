c0460017b59f2ce83c5addd3ec684552c7c04c7d
tukembaev <tukembaev.arif@gmail.com>
2026-03-27 16:58:05 +0600

    add: go_axios route changes

--- src/shared/config/go_axios.ts
+++ src/shared/config/go_axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const apiClientGo = axios.create({
-  baseURL: 'http://localhost:8080/api/v1/',
+  baseURL: 'https://uadmin.kstu.kg/task/api/v1/',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
